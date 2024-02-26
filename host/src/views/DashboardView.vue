d8dcb4d7cdb3002267a4db44d4dcd86f497152bc
llelardeux <laurent.lelardeux@soprasteria.com>
2024-02-26 13:29:29 +0100

    wireframing in progress

--- host/src/views/DashboardView.vue
+++ host/src/views/DashboardView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import WidgetDashboard from "../components/WidgetDashboard.vue"
+import WidgetChart from "../components/WidgetChart.vue"
+import WidgetDatagrid from "../components/WidgetDatagrid.vue"
 </script>
 
 <template>
@@ -13,4 +15,18 @@ import WidgetDashboard from "../components/WidgetDashboard.vue"
     </v-row>
   </v-container>
   <h2>Lorem ipsum</h2>
+  <v-container class="pa-0 mb-4">
+    <v-row>
+      <v-col>
+        <v-card height="350px">
+          <WidgetChart />
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card height="350px">
+          <WidgetDatagrid />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
